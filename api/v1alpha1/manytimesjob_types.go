59eb7a00b2f785153e88edbaa2b9c0f8351aecc2
ShotaKitazawa <skitazawa1121@gmail.com>
2020-02-24 15:35:42 +0900

    config types & make manifests

--- api/v1alpha1/manytimesjob_types.go
+++ api/v1alpha1/manytimesjob_types.go
@@ -17,25 +17,44 @@ limitations under the License.
 package v1alpha1
 
 import (
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ManyTimesJobSpec defines the desired state of ManyTimesJob
 type ManyTimesJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:Default=false
 
-	// Foo is an example field of ManyTimesJob. Edit ManyTimesJob_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Default false, true when job finished.
+	// This field is requirement for apply to same name Job resource.
+	// +optional
+	Done bool `json:"done,omitempty"`
+
+	// Specifies the job that will be created when executing a CronJob.
+	JobTemplate batchv1beta1.JobTemplateSpec `json:"jobTemplate"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// The number of successful finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// The number of failed finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
 // ManyTimesJobStatus defines the observed state of ManyTimesJob
 type ManyTimesJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+
+	// A list of pointers to currently running jobs.
+	// +optional
+	Active []corev1.ObjectReference `json:"active,omitempty"`
 }
 
 // +kubebuilder:object:root=true
